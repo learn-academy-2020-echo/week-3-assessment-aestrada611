c3fd2a5f23ee0a54603e8708e8375444b2d80e03
aestrada611 <>
2020-11-23 09:12:47 -0800

    Completed best of my ablility

--- code-challenges.js
+++ code-challenges.js
@@ -2,6 +2,84 @@
 
 
 // --------------------1) Create a function that returns the first 10 numbers of the Fibonacci sequence in an array. Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+//
+
+//I need the logic to take the number and add the previous two values to try to get a sum of the new value.
+//I then have to take that number and it to the array.
+//I then have to take the last two numbers and keep the process going.
+// //maybe create an empty array and .push values into it.
+//
+
+
+//I had a hard time with this one and I'm pretty sure that I was able to get very close to solving
+
+
+
+
+var fibonacciArray = [0]
+//
+//
+
+// const sumLog = (array) => {
+//   let addedArray = array.map((value, index) => {
+//     if(index === 0){
+//       return value
+//     } else{
+// //might have to add if statment to address if first value to just drop the value
+//     return value + array[index - 1 ] //[index - 1]
+// }
+//   })
+//   return addedArray
+// }
+
+const fibonacci = (array) => {
+  let newArray = []
+  for(let i = 0; i <= 10; i++){
+    if(i === 0){
+      newArray.push(array[i])
+    } else if (i > 0) {
+      newArray.map((value,index) => {
+  return value + array[index - 1]
+}).push(array[i])
+//newArray +
+    //  newArray.push(array[i])
+    }
+  }
+  return newArray
+}
+
+console.log(fibonacci(fibonacciArray));
+
+//
+// let mappedArray = fibonacci.map((value, index, array) => {
+//   if(index === 0){
+//     return value
+//   } else
+// })
+// }
+
+
+
+
+
+
+// const numberReturn = (num1, num2) => {
+// let current = 0
+// let next = 1
+// for (i = 0; i <= 10; i++)
+//   //if(num === 1 || num === 0)
+//   return fibonacciArray.push(num)
+// }
+//
+// console.log(numberReturn(2));
+//
+//   // if (num <= 1 && num > 0) {
+  //   return num
+  // }
+  // return numberReturn[num-1] + numberReturn[num - 2]
+  //let
+//  let secondSum = num2 + sum
+
 
 
 
@@ -13,6 +91,38 @@ var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
 
+//need a number sotring function
+const oddNumberSorter = (array) => {
+  //need to filter for numbers
+  let filteredArray = array.filter(value =>{
+    return typeof value === 'number'
+  })
+  //need to sort and filter for odd numbers only
+  let sortedArray = filteredArray.sort(function (a, b) {return a-b})
+  let oddsOnly = sortedArray.filter(value =>{
+    return value % 2 !== 0
+  })
+  return oddsOnly
+}
+
+//
+// const oddNumberSorter = (array) => {
+//   let filteredArray = array.filter(value =>{
+//     return typeof value === 'number'
+//   }).sort(function (a, b) {
+//     return a-b}).filter(value =>{
+//     return value % 2 !== 0
+//   })
+//   return filtredArray
+// }
+//
+
+
+
+console.log(oddNumberSorter(fullArr1));
+console.log(oddNumberSorter(fullArr2));
+
+
 
 
 
@@ -22,12 +132,76 @@ var middleLetters1 = "hello"
 // Expected output: “l”
 var middleLetters2 = "rhinoceros"
 // Expected output: “oc”
+var middleLetters3 = "yeot"
+var middleLetters4 = "herliktomt"
+var middleLetters5 ="thisisanevennumber"
+//the funtion takes in stirngs that are words
+//if word is even return two middle middleLetters
+//else if return one middle letters
+//.length in order to count the the middle
+//use charAt() to get the middle letter
+
+//function takes in strings
+const middleFinger = (string) => {
+  //need to find if string length is odd or even
+  let lengthFinder = string.length
+  if(lengthFinder % 2 != 0){
+    //return charAt for the middle
+    return string.charAt(lengthFinder *.5)
+  }else{
+    //return the middle letter and the one before that for two middle ones
+    return string.substring(lengthFinder * .5 - 1, lengthFinder * .5) + string.charAt(string.length*.5)
+    //substring
+  }
+}
 
 
 
+
+
+
+// const middleFinder = (string) => {
+//   let stringer = string.length
+//   if(string.length % 2 === 0 ){
+//     return charAt(string.length)
+//   } else if(string.length % 2 !== 0){
+//     return "yeet"//charAt(string.length * .5 && string.length *.5 + 1)
+//   }
+// }
+//
+ console.log(middleFinger(middleLetters1));
+ console.log(middleFinger(middleLetters2));
+ console.log(middleFinger(middleLetters3));
+ console.log(middleFinger(middleLetters4));
+ console.log(middleFinger(middleLetters5));
+//
 
 // --------------------4) READ CAREFULLY: Create a CLASS to get the area of a sphere. Create THREE spheres with different radi as test cases. Area of a sphere =  4πr^2 (four * pi * radi squared)
 
+//the four and pie reamin constant
+//need to create a variable for radisu and must make radius multiply with itself
+//need to understand what classes are and how they work
+
+//need a class
+//pi and four are constant
+class Area{
+  constructor(constant, radius){
+    this.constant = 12.56
+    this.radius = radius
+  }
+  //method will carry out all the math.
+  area (){
+    return `${this.constant * this.radius * this.radius}`
+ }
+}
+var sphere = new Area(12.56, 5)
+var sphere2 = new Area(12.56, 10)
+var sphere3 = new Area(12.56, 21)
+var sphere4 = new Area(12.56, 15)
+console.log(sphere.area());
+console.log(sphere2.area());
+console.log(sphere3.area());
+console.log(sphere4.area());
 
 
 
@@ -39,3 +213,34 @@ var numbersToAdd2 = [0, 7, -8, 12]
 // Expected output: [0, 7, -1, 11]
 var numbersToAdd3 = []
 // Expected output: []
+
+
+
+
+//I got this function to add the two previous numbers but had a hard time adding the value of the running sum instead of adding to the previous answers.
+
+
+
+
+//function that takes in array and returns the accumulating sum
+const sumLog = (array) => {
+  let addedArray = array.map((value, index) => {
+    if(index === 0){
+      return value
+    } else{
+//might have to add if statment to address if first value to just drop the value
+    return value + array[index - 1 ] //[index - 1]
+}
+  })
+  return addedArray
+}
+
+
+// const sumLogLoop = (array) => {
+// for(let i = 0; i < array.length); i++)
+// }
+
+
+console.log(sumLog(numbersToAdd1));
+console.log(sumLog(numbersToAdd2));
+console.log(sumLog(numbersToAdd3));
--- interview-questions.md
+++ interview-questions.md
@@ -9,81 +9,81 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is object destructuring?
 
-  Your answer:
+  Your answer: Destructuring is a way shorter way to access different values from objects.
 
-  Researched answer:
+  Researched answer: It is a syntax expression that makes it possible to unpack values from arrays, or properties from objects, into distinct new variables. This allows for code to be much more concise and readable.
 
 
 
 2. What are React lifecycle methods? Provide three examples.
 
-  Your answer:
+  Your answer: There are three main phases to react component's lifecycle which are mounting, updating, and unmounting.
 
-  Researched answer:
+  Researched answer: For mounting React has four built in methods and the one that is called before everything else is called the constructor and is where initial state is set. Render is always required and is what will actually be rendered on the page. For updating there are other methods but render is always required. There is only one unmounting method which is componentWillUnmount
 
 
 
-3. What is the difference between a class and an object?
+3.//// What is the difference between a class and an object?
 
-  Your answer:
+  Your answer: I know that they have different ways to write them out and objects are enclosed in bracket parenthesis.
 
-  Researched answer:
+  Researched answer: The main difference between the two is how they are written out and class declarations are not hoisted. A class also defines object properties and can also describe object behavior.
 
 
 
 4. What is the difference between a HTML div and a span?
 
-  Your answer:
+  Your answer: A div encloses everything within the div tags while a span tag is used within to wrap less text.
 
-  Researched answer:
+  Researched answer: A div is a block level element while a span is an inline element that should be used to wrap small portions of text.
 
 
 
 5. What is React.Fragment (or <>) and why would you want to use it?
 
-  Your answer:
+  Your answer: They are used to enclose all the pieces of code that are actually going to be rendered onto the page. Only code found between those tags will be read as JSX.
 
-  Researched answer:
+  Researched answer: These tags are used to return elements in React. These tags exist as a workaround to return adjacent JSX elements onto the page.
 
 
 
 6. What does it mean to create a responsive web application? What are three options for creating responsive design?
 
-  Your answer:
+  Your answer: A responsive web application is one that has different parts that can render when specified conditions are met.
 
-  Researched answer:
+  Researched answer: A responsive web app is one that has interactivity and has layouts that adjust to fit the screen they are being displayed on. Some options to create responsive design are bootstrap, wirefy, and Invision.
 
 
 
 7. What are props in React?
 
-  Your answer:
+  Your answer: Props is the argument that the super and constructor take in order to pass properties to other components.
 
-  Researched answer:
+  Researched answer: Props is a special keyword that has stands for properties and is used in order to send data from a parent component to a child component.
 
 
 
 8. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: It is used to delcare state and has to do with scope but other then that I dont really know much more.
 
-  Researched answer:
+  Researched answer: This is mostly used in regards to scope. This refers to the objet when calling a function.
 
 
 
 9. STRETCH: What is a ternary operator in JavaScript?
 
-  Your answer:
+  Your answer: It is like a shortcut for if/else statements.
 
-  Researched answer:
+  Researched answer: If there are only two possible outcomes then it makes a great place to use ternary. A ternary has three parts which are the evaluating condition, a question mark and a semicolon that separates truthy and falsey outcomes.
 
 
 
 10. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: A type of function that has no name and does not have an argument.
 
-  Researched answer:
+  Researched answer: An anonymous function is one without a name and cannot be invoked again after its creation. They are often used as arguments for other functions.
 
 
 
